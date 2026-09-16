19b48a586217f2a2031cc79463828d255e3dead2
Iago1203 <iagocadinelli1@gmail.com>
2026-09-16 16:53:49 -0300

    Add files via upload

---
+++ POO/CadastroAcademia/src/academia/AlunoAcademia.java
@@ -0,0 +1,28 @@
+package academia;
+
+public class AlunoAcademia {
+
+	String nome;
+	int idade;
+	double peso;
+	double altura;
+	
+	public AlunoAcademia(String nome, int idade, double peso,double altura) {
+		this.nome = nome;
+		this.idade = idade;
+		this.peso = peso;
+		this.altura = altura;
+	}	
+	public static boolean ehMenorDeIdade(int idade) {
+		if  int idade = (<18) {
+			return  true;
+	}	else {
+		return  false;
+	} public static boolean exibirDados(double peso, double altura) {
+		return peso/ (altura * altura);
+	}
+	}
+	
+
+	
+}
